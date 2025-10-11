8e0f3cd75699d3c864b9c87f04f6e4e4e9509b5c
The Daxie Coder <isaac.d.ritchie@gmail.com>
2025-10-11 23:38:26 +0100

    week 1 lab coursework

---
+++ week1_lab.py
@@ -0,0 +1,20 @@
+print ("hello, world!")
+
+
+#storing a text variable (string)
+StudentName = "Isaac Ritchie"
+
+#storoing a number variable (integer)
+StudentAge = 20
+
+#pring variables to the console
+print ("StudentName", StudentName)
+print ("StudentAge", StudentAge) 
+
+
+#simple calculations
+num1 = 10
+num2 = 15
+total = num1 + num2
+
+print ("the sum is", total)
