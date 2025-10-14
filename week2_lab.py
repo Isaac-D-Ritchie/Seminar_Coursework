650198b1f879de26f7ad1721a290cd463f10eeff
The Daxie Coder <isaac.d.ritchie@gmail.com>
2025-10-14 14:25:07 +0100

    added further code annotations

--- week2_lab.py
+++ week2_lab.py
@@ -8,4 +8,9 @@ DegreesC = float(input())
 #this converts the DegreesC variable to Fahrenheit using the formula (C * 9/5) + 32 and sets it as the variable DegreesF
 DegreesF = (DegreesC * 9/5) + 32
 
+#this print statement prints both variables in a 'simplified' f-string format
 print (f"The temperature in celsius is {DegreesC}°C and in Fahrenheit it is {DegreesF}°F")
+
+""""
+here is the DegreesC is converted it takes the original value and converts it to Fahrenheit using the formula given. As degreesC is a float variable it can take decimal values and the result of the conversion will also be a float value. goes up so does DegreesF
+"""
