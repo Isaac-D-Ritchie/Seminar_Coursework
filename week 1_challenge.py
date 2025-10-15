ebe53a4e6c2d38633805efc9e1be90c5cb64023c
The Daxie Coder <isaac.d.ritchie@gmail.com>
2025-10-15 22:36:24 +0100

    Update week 1_challenge.py

--- week 1_challenge.py
+++ week 1_challenge.py
@@ -8,12 +8,13 @@ location = input("Enter a location... ")
 BoxLines = (f"Name: {name}", f"Username: {username}", f"Location: {location}")
 
 #This determines the maximum length by temirearily assigning each item on the list in the BoxLines variable and checks for the maximum lenngth to save it as Line max length
-#I later add 3 to create a buffer between the longest line in the box and its border
-LineMaxLength = 3 + (max(len(BoxLines))
+#I later add 5 to create a buffer between the longest line in the box and its border
+LineMaxLength = 5 + (max(len(Lines) for Lines in BoxLines))
 
 #here i print the edges of the box "* +" and the "-" as many times as the variable LineMaxLength to create the top of the box
 print ("*" + "-" * LineMaxLength + "+")
-#next i used a for loop to print 
-for BoxLines:
-    print (f"| {BoxLines.ljust(LineMaxLength)}  |")
-print ("+" + "-" * LineMaxLength + "*")
+#next i used a for loop to print each line in the Boxlines i ajusted it to the left using .ljust by the amount of the max length. i later put - 2 to the border didnt quite line up while the longest text detection still needed leeway incase someones name was the longest input and the other inputs were short the box alignment could be incorrect so the adition of 5 ariginally negates this
+for Lines in BoxLines:
+    print (f"| {Lines.ljust(LineMaxLength - 2)} |")
+#this prints the bottom of the box display
+print ("*" + "-" * LineMaxLength + "+")
