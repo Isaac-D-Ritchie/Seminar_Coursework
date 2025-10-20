9821a00165d1ae976853025ea5830de4f8fe15d5
The Daxie Coder <isaac.d.ritchie@gmail.com>
2025-10-20 20:23:31 +0100

    "feat: i have added code asking for the users name and age and storing the age as int nd float."

---
+++ Test1_Prep2 (indipendednt).py
@@ -0,0 +1,27 @@
+#For the porposes of this preparation i have genrated a question via chatgpt.com whihc covers the perameters of Test 1.
+
+#write a Python program that:
+# 1) Start with a comment decribing the program.
+# 2) Asks the user to enter their name and age.
+# 3) Stores age as both an int and a float.
+# 4) Uses string oporator + to print a greeting with their name.
+# 5) Use the arithmatic oporator % to check if their age is an even or odd number. 
+# 6) Use if/elif/else to:
+#   * Print "You are a teenager" if their age is between 13 and 19.
+#   * Print "You are an adult" if 20 or older.
+#   * Otherwise, print "You are a a child".
+# 7) Use the logical oporator (and) to check if their age is between 30 and 39 (inclusive) and print "You are in your 30s!" if true.
+# 8) Write a function called countdown(age) that uses a for loop with range() to pint numbers from the users age down to 1 Call it the user age.
+# 9) Call that function with the user age (integer)
+
+
+
+#In this program i will be asking for the users name and age. Then it will welcome them displaying their name and outputting if they are a teenager, child, or adult. 
+#I will output if they are in their 30s and then a countdown beginning at their age down to 1.
+
+#here i am asking the user to input their name and age then save them and covert their age to a int and a float.
+name = print (input("What if your name? "))
+age = print (input("What is your name? "))
+
+age_integr = int(age)
+aga_float = float(age)
