646fe3dacfbb280bb059c597f2f810754ae9098c
The Daxie Coder <isaac.d.ritchie@gmail.com>
2025-10-21 14:28:23 +0100

    I  have chnaged the counter to count from 1 - 20 but obly output the ven numbers

--- week3_lab.py
+++ week3_lab.py
@@ -3,5 +3,7 @@ This file contains the code for week 3 lab excercises,
 focusing on for loops, while loops, and GitHub intergration.
 """
 
-for i in range(10):
+#This prints only the 
+for i in range(0, 21, 2):
     print(f"\n {i + 1}")
+
