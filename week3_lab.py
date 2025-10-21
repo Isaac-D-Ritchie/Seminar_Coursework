fc1f3b59ab94f01437c65048a876c32f8074acac
The Daxie Coder <isaac.d.ritchie@gmail.com>
2025-10-21 14:40:59 +0100

    Feat: I have added a costs counter

--- week3_lab.py
+++ week3_lab.py
@@ -3,7 +3,17 @@ This file contains the code for week 3 lab excercises,
 focusing on for loops, while loops, and GitHub intergration.
 """
 
-#This prints only the 
-for i in range(0, 21, 2):
-    print(f"\n {i + 1}")
+#This prints only the even numbers between 0 and 20
+#for i in range(0, 20, 2):
+ #   print(f"\n {i + 1}")
 
+
+#This code
+costs= [15.00, 12.50, 3.75, 40.25]
+total_cost = 0
+
+for cost in costs:
+    total_cost = total_cost + cost
+    print(f"£{total_cost}")
+
+print (f"Your total cost is £{total_cost: .2f}")
