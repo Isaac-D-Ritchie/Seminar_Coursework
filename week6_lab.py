322494aca886d8baa6cc04810dfc1eda36fe6ce0
The Daxie Coder <isaac.d.ritchie@gmail.com>
2025-11-12 17:25:53 +0000

    Feat: I have completed lab exercise 2

--- week6_lab.py
+++ week6_lab.py
@@ -16,7 +16,9 @@ def take_damage(health):
 player_health = take_damage(player_health)
 print(f"Player health is: {player_health}")
 
-#Now i am completing the lab exercises
+
+
+#lab exercises:
 #Exercise 1 - String slicing and .split()
 log_data = "SCORE:Player_1:2500"
 
@@ -25,3 +27,28 @@ parced_data = log_data.split(":")
 #I have also used f strings to change the output format
 print (f"Player name: {parced_data[1]}")
 print (f"Player score: {parced_data[2]}")
+
+#Exercise 2 - create a quiz using two lists
+
+#List of questions and answers saved as a variables.
+questions = ["What is 2+2?..","What is that captial of france?..","What keyword defines a function in python?.."]
+answers = ["4","paris","def"]
+#score variable
+score = 0
+
+#For loop to ask quesrions and compare them to the corespndinf answer
+for q in range(len(questions)):
+    user_answer = input(f"{questions[q]}")
+    if user_answer == answers[q]:
+        print("correct! +1 score")
+        score += 1
+        continue
+    else:
+        print("wrong answer :(")
+        continue
+
+#Prints the final score
+print(f"Score = {score}")
+
+
+
