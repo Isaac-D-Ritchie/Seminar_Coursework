72e1aa81e9d7dce6f3e42bdf048984eb5ae82c7f
The Daxie Coder <isaac.d.ritchie@gmail.com>
2025-11-14 14:26:29 +0000

    Feat: I have completed the challenge with my own ajustments for future develment ease

--- week6_lab_challenge.py
+++ week6_lab_challenge.py
@@ -12,21 +12,21 @@ it must check if a student has the correct prerequisites before they are allowed
 #Enrolemt check
 def can_enrol(student_id, module_code):
     if student_id in student_database:
-        if module_code in module_id:
+        if module_code in student_database[student_id]:
+            print(f"Student already enrolled in {module_code}")
+            return False
+        elif module_code in module_id:
             module_prerequisites_set = set(module_id.get(module_code)[1:])
             student_modules_set = set(student_database.get(student_id)[1:])
             student_eligable = True
 
             for module_required in module_prerequisites_set:
                 if module_required in student_modules_set:
-                    print(f"module {module_required} achived")
+                    print(f"Prerequisite module {module_required} achived")
+                    return True
                 else:
-                    print(f"moule {module_required} not achived")
-                    student_eligable = False
-            if student_eligable is True:
-                return True
-            else:
-                return False
+                    print(f"Prerequisite module {module_required} not achived")
+                    return False
         else:
             print("Module code not found.")
             return False
@@ -35,7 +35,29 @@ def can_enrol(student_id, module_code):
         print("Student not found.")
         return False
 
-
+#Enroll student after data input
+def enrol_student(student_id, module_code):
+        if can_enrol(student_id, module_code) == True:
+            student_database[student_id].append(module_code)
+            print(f"Student sucsesfully enrolled in {module_code}")
+            print(f"\n{student_database.get(student_id)}\n")
+            return True
+        else:
+            print("Enrolment unsucsesful, Try again...")
+            return False
+            
+#function while loop for enrolling student
+def student_enroll_try():
+    while True:
+        student_id_input = input("Input a student ID...")
+        module_request_input = input("Input module for student enrolment...\n")
+        if enrol_student(student_id_input, module_request_input) == True:
+            if input("Press enter to continue or 'q' to end search.") == "q":
+                break
+            else:
+                continue
+        else:
+            continue
 
 #Module ID Dictionary prerequisites
 module_id = {"CIS1702" : ["Programming 1"], "CIS1000" : ["Data Structures", "CIS1702"],
@@ -47,7 +69,17 @@ student_database = {"s12345" : ["Alice", "CIS1702", "MTH1001"],
                     "s67890" : ["Bob", "CIS1702", "CIS1001"],
                     "s54321" : ["Charlie", "MTH1001"]}
 
-
-#Testing
-student_eligabliity = can_enrol("s12345", "CIS2000")
-print(f"Student eligability, {student_eligabliity}")
+#Menu for using the database
+while True:
+    menu_input = input("DATABASE MENU\nSelect from the following options:\nEnrol Student: e\nDisplay modules: m\nDisplay Student Info: s \nQuit Menu: q\n ")
+    if menu_input == "e":
+        student_enroll_try()
+    elif menu_input == "m":
+        for modules in module_id:
+            print(f"Module name: {module_id.get(modules)[:1]}\nModule requirements:{module_id.get(modules)[1:]}\n")
+            continue
+    elif menu_input == "s":
+        for students in student_database:
+            print(f"Student name: {student_database.get(students)[:1]}\nModules achived: {student_database.get(students)[1:]}\n")
+    elif menu_input == "q":
+        break
