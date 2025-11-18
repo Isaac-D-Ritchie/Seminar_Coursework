da67f042ef5be183de94ebebabeea01272c72b18
The Daxie Coder <isaac.d.ritchie@gmail.com>
2025-11-18 15:07:03 +0000

    Feat: I have completed exercise 1; 1.1, 1.2, and 1.3

--- week7_lab.py
+++ week7_lab.py
@@ -9,7 +9,7 @@ I will show creating, reading, writing, and ammeding files.
 #This function asks for input and cretes a file with that text or overrides the file if it already exists.
 def write_new_diary_entry():
     new_entry = input("Write new diary entry.\n")
-    with open("week7_diary.txt", "w") as diary:
+    with open("week7_diary.txt", "a") as diary:
         diary.write(new_entry + "\n") #prints and "\n" moves to next line.
         return
 #Calls function
@@ -20,4 +20,4 @@ def read_diary():
     with open("week7_diary.txt", "r") as diary:
         return diary.read()
 #Calls function
-print(read_diary())
+print("\n" + read_diary())
