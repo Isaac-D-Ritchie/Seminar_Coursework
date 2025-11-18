8b618578ff557591b87b245ad34dbe51ecf55d3f
The Daxie Coder <isaac.d.ritchie@gmail.com>
2025-11-18 21:32:06 +0000

    Feat: I have finished exercise 2.2

--- week7_lab.py
+++ week7_lab.py
@@ -25,13 +25,32 @@ def read_diary():
 #Exercise 2 - Working with CSV
 import csv
 
-gradelist = {
+gradelist = { #Dataset for csv
     "Alice":[85,90,92],
     "Bob":[78,81,85],
     "Charlie":[95,100,98]
 }
-with open("gradebook.csv", "w", newline="") as gradebook:
-    writer = csv.writer(gradebook) #This sets a varaible to the csv command to prevent syntax issues
-    writer.writerow(["student", "grade 1", "grade 2", "grade 3"])
-    for student, grades in gradelist.items():
-        writer.writerow([student] + grades)
+#This function creates/writes to a csv using the gradelist 
+def write_csv():
+    with open("week7_gradebook.csv", "w", newline="") as f:
+        writer = csv.writer(f) #This sets a varaible to the csv command to prevent syntax issues
+        writer.writerow(["student", "grade 1", "grade 2", "grade 3"])
+        for student, grades in gradelist.items():
+            writer.writerow([student] + grades)
+    return
+#Calls function
+write_csv()
+
+#Function to read csv file and print the students name and average score
+def average_from_gades():
+    with open("week7_gradebook.csv", "r") as f:
+        reader = csv.reader(f)
+        next(reader) #skips the title row
+        for row in reader:
+            temp_grade_list = {int(c) for c in row[1:]} #creates temporary list of grades for each row
+            average_grade = sum(temp_grade_list) / len(temp_grade_list) #calculates the average score from the list
+            print(f"{row[0]}: Average grade = {average_grade:.1f}\n")
+#Calls function
+average_from_gades()
+
+
