a695434913d6d59d43d921b64103563e2d9360c7
The Daxie Coder <isaac.d.ritchie@gmail.com>
2025-11-18 15:35:12 +0000

    Feat:I have completed Exercise 2.1

--- week7_lab.py
+++ week7_lab.py
@@ -13,11 +13,25 @@ def write_new_diary_entry():
         diary.write(new_entry + "\n") #prints and "\n" moves to next line.
         return
 #Calls function
-write_new_diary_entry()
+#write_new_diary_entry()
 
 #This function reads the diary file and prints the result
 def read_diary():
     with open("week7_diary.txt", "r") as diary:
         return diary.read()
 #Calls function
-print("\n" + read_diary())
+#print("\n" + read_diary())
+
+#Exercise 2 - Working with CSV
+import csv
+
+gradelist = {
+    "Alice":[85,90,92],
+    "Bob":[78,81,85],
+    "Charlie":[95,100,98]
+}
+with open("gradebook.csv", "w", newline="") as gradebook:
+    writer = csv.writer(gradebook) #This sets a varaible to the csv command to prevent syntax issues
+    writer.writerow(["student", "grade 1", "grade 2", "grade 3"])
+    for student, grades in gradelist.items():
+        writer.writerow([student] + grades)
