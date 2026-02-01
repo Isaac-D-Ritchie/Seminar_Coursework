e5871f06ae9a01aa504efa35947537f31dbf3cc5
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-02-01 12:58:14 +0000

    Added exercise information and given code with added inputs

---
+++ Semester_2/pre-module_warm-up/Exercise_2.py
@@ -0,0 +1,24 @@
+"""
+Pre-module warm-up exercise 2
+
+Scenario:
+A function formats user information, but it does not specify
+input or return types.
+
+Task:
+Rewrite the function signature to include correct type hints.
+
+Requirements:
+# name must be a string.
+# age must be an integer.
+# The function must declare that it returns a string.
+"""
+
+#Before
+
+def format_user(name, age):
+    return f"User {name} is {age} years old."
+
+input_name = "Isaac"
+input_age = 21
+print(format_user(input_name, input_age))
