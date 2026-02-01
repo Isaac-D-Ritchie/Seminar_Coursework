6f2695edcf3bbb708d37584f921ba0f2add38607
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-02-01 02:43:58 +0000

    warm-up exercise 1

---
+++ Semester_2/pre-module_warm-up/Exercise_1.py
@@ -0,0 +1,23 @@
+"""
+Pre-module warm-up exercise 1
+
+Scenario:
+You are given a function that correctly calculates a value, 
+but incorrectly prints the result instead of returning it.
+
+Task:
+Modify the function so that it returns the calculated value instead of printing it.
+
+Requirements:
+Change only one word inside the function.
+Do not add new variables or additional print statements
+"""
+
+#Before
+
+def double_number(value):
+    result = value * 2
+    print(result)
+ 
+my_num = double_number(10)
+print(f"The number is {my_num}")
