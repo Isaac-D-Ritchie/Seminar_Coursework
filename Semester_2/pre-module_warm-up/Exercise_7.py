9dac0ddec2e53264e001467c934f569be82efe6b
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-02-04 02:02:26 +0000

    Completed exercise 7

--- Semester_2/pre-module_warm-up/Exercise_7.py
+++ Semester_2/pre-module_warm-up/Exercise_7.py
@@ -19,9 +19,72 @@ Add:
 #A docstring explaining the function’s purpose
 
 """
+
+"""
 #Before
  
 def calculate_vat():
     price = float(input("Enter price: "))
     vat = price * 0.2
     print(f"VAT is {vat}")
+"""
+
+#After - completed task as per requirements, with error catching
+#Extra - Added reuable function for getting positive float
+
+
+#Function to get valid input as float
+def get_valid_positive_float() -> float:
+    """
+    Gets a valid positive float input
+
+    Arguments:
+        None
+
+    Returns:
+        validated float value
+
+    Raises:
+        ValueError, EOFError, KeyboardInterrupt
+    """
+    while True:
+        try:
+            value = float(input("Input price £:").strip())
+            if not value:
+                raise ValueError
+            elif value < 0:
+                raise ValueError
+            return value
+    
+        except(ValueError, EOFError, KeyboardInterrupt):
+            return None
+
+
+#Function to get VAT
+def get_vat(price: float) -> float:
+    """
+    Calculates 20% (VAT) of float input
+
+    Arguments:
+        float input for calculation
+
+    Returns:
+        calculated 20% of input as float
+    """
+    try:
+        vat: float = price * 0.2
+        return vat
+    
+    except ValueError:
+        return None
+    
+
+#CLI - interface
+while True:
+    try:
+        price = get_valid_positive_float()
+        vat: float = get_vat(price)
+        print(f"VAT = {(vat):.2f}")
+
+    except (ValueError, TypeError):
+        print("Error, try again")
