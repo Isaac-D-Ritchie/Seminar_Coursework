d4fa9d207dec8c66f7ab1bc8d99aace4f91d1598
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-02-05 14:20:45 +0000

    Added function to get valid integer input within an optional range

--- Reuseable_Functions.py
+++ Reuseable_Functions.py
@@ -9,7 +9,7 @@ These functions are for input sanitation and validation
 
 
 #Get safe input
-def safe_input(prompt) -> str:
+def safe_input(prompt: str) -> str:
     """
     Function to validate safe input
     Arguments:
@@ -26,7 +26,7 @@ def safe_input(prompt) -> str:
 
 
 #Get non-empty string
-def get_non_empty_string(prompt) -> str:
+def get_non_empty_string(prompt: str) -> str:
     """
     Function to get non empty string using while loop
     Arguments:
@@ -46,8 +46,45 @@ def get_non_empty_string(prompt) -> str:
 
 
 
+#Get valid integer with optional range
+def get_valid_integer(prompt: str, min_value: int = None,
+                       max_value: int = None) -> int:
+    """
+    Function to get a valid inter within a range if given
+    Arguments:
+        prompt - string for input prompt
+        min_value - minimum integer value
+        max_value - maximum integer value
+    Returns:
+        valid integer
+    """
+    while True:
+        raw_input: str = safe_input(prompt)
+        sanitized_input: str = raw_input.strip()
+
+        if not sanitized_input:
+            print("\nError: Input cannot be empty")
+            continue
+
+        try:
+            int_value = int(sanitized_input)
+        except ValueError:
+            print("\nError: Invalid integer")
+            continue
+
+        if min_value is not None and int_value < min_value:
+            print(f"\nError: Value must be between {min_value} and {max_value}")
+            continue
+        if max_value is not None and int_value > max_value:
+            print(f"\nError: Value must be between {min_value} and {max_value}")
+            continue
+
+        return int_value
+        
+
+
 #Execute if file is run directly
 if __name__ == "__main__":
     print("Start Program")
     prompt = "Input:"
-    print(get_non_empty_string(prompt))
+    print(get_valid_integer(prompt, -99, 99))
