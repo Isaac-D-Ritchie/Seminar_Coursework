395699b91cf048b3aa057aaaa8d5ba8dda709a72
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-02-05 05:45:52 +0000

    Added 'get non-empty string' function

--- Reuseable_Functions.py
+++ Reuseable_Functions.py
@@ -7,6 +7,8 @@ These functions are for input sanitation and validation
 
 """
 
+
+#Get safe input
 def safe_input(prompt) -> str:
     """
     Function to validate safe input
@@ -21,6 +23,19 @@ def safe_input(prompt) -> str:
         print("\nInput Error")
         return ""
 
+
+
+# Get non-empty string
+def get_non_empty_string(prompt) -> str:
+    while true:
+        raw_input = safe_input(prompt)
+        sanitized_input = raw_input.strip()
+
+        if sanitized_input == "":
+            print("Error: Input cannot be empty")
+            continue
+
+    return sanitized_input
 
 
 
