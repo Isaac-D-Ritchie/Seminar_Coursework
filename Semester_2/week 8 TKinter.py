e50e12415c04dfd01afd7230569b67606567f3bd
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-03-18 00:28:16 +0000

    Started lab 8

---
+++ Semester_2/week 8 TKinter.py
@@ -0,0 +1,24 @@
+"""
+Isaac Ritchie
+
+Week 8 - Graphical User Interfaces with Python tkinter
+
+1.1 Your First tkinter Window
+1.2 Labels and Text Display
+1.3 Buttons and Event Handling
+1.4 Entry Widgets and User Input
+1.5 Layout Managers: pack, grid, place
+1.6 Checkbuttons, Radiobuttons and Variables
+1.7 Listbox and Scrollbar
+1.8 Menus and Message Boxes
+1.9 Frames and Organizing Your Interface
+1.10 Building a Complete Application: Student Record Manager
+"""
+
+
+""" Imports """
+import tkinter as tk
+
+
+""" 1.1 First tkinter window """
+
