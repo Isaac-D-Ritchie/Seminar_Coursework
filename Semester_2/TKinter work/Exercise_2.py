c58548011ad3198f338c1f138a6b10d2abdea43e
The Daxie Coder <isaac.d.ritchie@gmail.com>
2026-03-23 00:57:36 +0000

    Added info and file for exercise 2

---
+++ Semester_2/TKinter work/Exercise_2.py
@@ -0,0 +1,20 @@
+"""
+Exercise 2 - Simple Calculator
+
+Difficulty: Beginner - Intermediate
+
+Build a calculator application with these features:
+
+    1. Two Entry widgets for entering numbers.
+
+    2. Four Buttons for the basic operations: Add (+), Subtract (-), 
+        Multiply (x), Divide(÷).
+
+    3. A Label to display the result.
+
+    4. Use grid() layout to arrange the widgets neatly in rows and columns.
+
+    5. Include error handling for division by zero and non-numeric input.
+
+    6. Display appropriate error messages using messagebox.showerror().
+"""
